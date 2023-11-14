31e79c49e6f09b378979043a8e3e688c16eb3a69
max <maximilian.esterhammer-fic@gamalon.com>
2023-11-14 14:18:22 -0600

    make redirects default to https

--- videobookmarks/__init__.py
+++ videobookmarks/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_talisman import Talisman
 
 DB_URL = os.getenv('DB_URL')
 if DB_URL is None:
@@ -48,4 +49,4 @@ def create_app(test_config=None):
     # the tutorial the tag_list will be the main index
     app.add_url_rule("/", endpoint="index")
 
-    return app
+    return Talisman(app)
