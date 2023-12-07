7eed3dfa7e3ea3742891b2584cbf50dd0c00303c
max <maximilian.esterhammer-fic@gamalon.com>
2023-12-07 10:41:50 -0600

    fix js

--- videobookmarks/static/index.js
+++ videobookmarks/static/index.js
@@ -1,13 +1,15 @@
 // handle buttons and form for adding tag
 const deleteButtons = document.getElementsByClassName('button big delete');
+var link;
 for (button of deleteButtons){
-    button.addEventListener('click', (event) => {
-        var link = button.value;
+    button.addEventListener('click', function (e) {
+        link = this.value;
         fetch(link, {
             method: 'DELETE',
             headers: {
               'Content-Type': 'application/json',
             }
         });
+        location.reload();
     });
 };
